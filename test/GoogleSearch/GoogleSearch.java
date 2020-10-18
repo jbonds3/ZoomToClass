7278f37b840dccc58971ca7a6a175f4a87afe93b
John Bonds III <johnwb0007@gmail.com>
2020-10-18 07:01:08 -0400

    Modified GoogleSearch.java to accept user input.

--- test/GoogleSearch/GoogleSearch.java
+++ test/GoogleSearch/GoogleSearch.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +13,12 @@ public class GoogleSearch {
 	public static void main(String[] args) {
 		  try {
 			  //Sets the webdriver.chrome.driver key to the absolute path below
-			  System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+			  
+			  System.setProperty("webdriver.chrome.driver", "C:\\Users\\JBonds\\Documents\\NSBE TORCH\\Selenium\\chromedriver_win32v85\\chromedriver.exe");
+			  
+			  BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			  System.out.print("What do you want to search?: ");
+			  String inputStr = input.readLine();
 			  
 			  //Instantiates a WebDriver object 
 			  WebDriver driver = new ChromeDriver();
@@ -20,10 +30,11 @@ public class GoogleSearch {
 			  Thread.sleep(5000);  // Let the user actually see something!
 			  
 			  
+			  
 			  WebElement searchBox = driver.findElement(By.name("q"));
 			  
 			  //sends string "ChromeDriver"
-			  searchBox.sendKeys("ChromeDriver");
+			  searchBox.sendKeys(inputStr);
 			  searchBox.submit();
 			  Thread.sleep(5000);  // Let the user actually see something!
 			  driver.quit();
@@ -31,7 +42,10 @@ public class GoogleSearch {
 			  System.out.println("Woah There Buddy!!!!");
 		  } catch (InvalidArgumentException e) {
 			  System.err.println("\n\nCheck your driver.get() argument!!!");
-		  }
+		  } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
